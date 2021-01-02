0d47cfdcdcfb6030c33d8f967eaea4150d13e70c
Mohammed Mudassir Ali <alimudassir12@gmail.com>
2021-01-02 18:27:59 +0530

    Implement UI and Functionality

---
+++ app.js
@@ -0,0 +1,45 @@
+let min = 4,
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
+
+const game = document.querySelector('#game'),
+  minNum = document.querySelector('.min-num'),
+  maxNum = document.querySelector('.max-num'),
+  guessBtn = document.querySelector('#guess-btn'),
+  guessInput = document.querySelector('#guess-input'),
+  message = document.querySelector('.message');
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+guessBtn.addEventListener('click', () => {
+  let guess = parseInt(guessInput.value);
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+  if (guess === winningNum) {
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
+  } else {
+    guessesLeft -= 1;
+    if (guessesLeft === 0) {
+      gameOver(false, `Game over, you lost. The correct number was ${winningNum}`);
+    } else {
+      guessInput.value = '';
+      setMessage(`Guess is incorrect. Guesses left : ${guessesLeft}`, 'red');
+    }
+  }
+});
+
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
+
+function gameOver(won, msg) {
+  let color = won ? 'green' : 'red';
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  message.style.color = color;
+  setMessage(msg);
+}
